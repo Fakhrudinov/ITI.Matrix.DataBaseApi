2d606d280901f6815ecafcd1669ca74529213c4d
Alexander F <asbuka@gmail.com>
2022-06-15 06:58:40 +0300

    Add db controller and repo

---
+++ ITI.Matrix.DB.API/ITI.Matrix.DB.API/Controllers/DataBaseController.cs
@@ -0,0 +1,139 @@
+﻿using DataAbstraction.Interfaces;
+using DataAbstraction.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.Matrix.DB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataBaseController : ControllerBase
+    {
+        private ILogger<DataBaseController> _logger;
+        private IDataBaseRepository _repository;
+
+
+        public DataBaseController(ILogger<DataBaseController> logger, IDataBaseRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet("CheckConnections/MatrixDataBase")]
+        public async Task<IActionResult> CheckConnection()
+        {
+            _logger.LogInformation("HttpGet CheckConnections/MatrixDataBase Call");
+
+            ListStringResponseModel result = await _repository.CheckConnections();
+
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Messages);
+            }
+        }
+
+        [HttpGet("GetUser/SpotPortfolios/{clientCode}")]
+        public async Task<IActionResult> GetUserSpotPortfolios(string clientCode)
+        {
+            _logger.LogInformation($"HttpGet GetUser/SpotPortfolios {clientCode} Call");
+
+            MatrixClientCodeModelResponse result = await _repository.GetUserSpotPortfolios(clientCode);
+
+            if (result.Response.IsSuccess)
+            {
+                if (result.MatrixClientCodesList.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Response.Messages);
+            }
+        }
+
+        [HttpGet("GetUser/FortsPortfolios/{clientCode}")]
+        public async Task<IActionResult> GetUserFortsPortfolios(string clientCode)
+        {
+            _logger.LogInformation($"HttpGet GetUser/FortsPortfolios {clientCode} Call");
+
+            MatrixToFortsCodesMappingResponse result = await _repository.GetUserFortsPortfolios(clientCode);
+
+            if (result.Response.IsSuccess)
+            {
+                if (result.MatrixToFortsCodesList.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Response.Messages);
+            }
+        }
+
+        [HttpGet("GetUser/FortsPortfolios/NoEDP/{clientCode}")]
+        public async Task<IActionResult> GetUserFortsNoEDPPortfolios(string clientCode)
+        {
+            _logger.LogInformation($"HttpGet GetUser/NoEDP/FortsPortfolios {clientCode} Call");
+
+            MatrixToFortsCodesMappingResponse result = await _repository.GetUserFortsPortfoliosNoEDP(clientCode);
+
+            if (result.Response.IsSuccess)
+            {
+                if (result.MatrixToFortsCodesList.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Response.Messages);
+            }
+        }
+
+        [HttpGet("Get/IsPortfolios/InEDP/{clientRfPortfolio}")]
+        public async Task<IActionResult> GetIsPortfolioInEDP(string clientRfPortfolio)
+        {
+            _logger.LogInformation($"HttpGet Get/IsPortfolios/InEDP/{clientRfPortfolio} Call");
+
+            BoolResponse result = await _repository.GetIsPortfolioInEDP(clientRfPortfolio);
+
+            if (result.IsSuccess)
+            {
+                if (result.Messages.Count > 0 && result.Messages[0].Equals("(404)"))
+                {
+                    return NotFound("(404) not found portfolio " + clientRfPortfolio);
+                }
+
+                if (!result.IsTrue)
+                {
+                    result.Messages.Add("False. Portfolio belong to EDP.");
+                }
+                else
+                {
+                    result.Messages.Add("Ok. Portfolio non EDP.");
+                }
+                
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result.Messages);
+            }
+        }
+
+        // get personal info
+        // check RF - not in MO
+    }
+}
---
+++ ITI.Matrix.DB.API/ITI.Matrix.DB.API/Program.cs
@@ -0,0 +1,38 @@
+using DataAbstraction.Connections;
+using DataAbstraction.Interfaces;
+using Matrix.DataBase;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//// add child Quik API connections adress
+//builder.Services.Configure<HttpClientConfig>(
+//    builder.Configuration.GetSection("HttpClientConfig"));
+
+// Matrix DataBase configure
+builder.Services.AddTransient<IDataBaseRepository, DataBaseRepository>();
+builder.Services.Configure<DataBaseConnectionConfiguration>(
+    builder.Configuration.GetSection("DataBaseConfig"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
